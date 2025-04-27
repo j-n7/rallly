f7a9af406ad12be40149e884aa8b9bec77d48942
Jan-Niklas <me@j-n7.dev>
2025-04-28 01:09:25 +0200

    Update poll-footer.tsx

--- apps/web/src/components/poll/poll-footer.tsx
+++ apps/web/src/components/poll/poll-footer.tsx
@@ -6,14 +6,14 @@ export function PollFooter() {
   return (
     <div className="pb-12 pt-4 text-center text-sm text-gray-500">
       <Trans
-        defaults="Powered by <a>{name}</a>"
+        defaults="Powered with 💚 by <a>{name}</a>"
         i18nKey="poweredByRallly"
-        values={{ name: "rallly.co" }}
+        values={{ name: "jinc.io" }}
         components={{
           a: (
             <Link
               className="hover:text-primary-600 rounded-none border-b border-b-gray-500 font-semibold"
-              href="https://rallly.co"
+              href="https://jinc.io"
             />
           ),
         }}
